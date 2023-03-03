2257af00349d4e76939acfc6e867c0c5d3ee4590
atlaskua <zylyn89@gmail.com>
2023-03-03 09:21:07 +0800

    initiated

---
+++ src/main/java/nus/iss/ssfbatch2exam/SsfBatch2ExamApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.ssfbatch2exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfBatch2ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfBatch2ExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/ssfbatch2exam/SsfBatch2ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.ssfbatch2exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfBatch2ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
